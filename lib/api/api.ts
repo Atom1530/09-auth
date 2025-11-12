c67a4f189f0f789765b8ffa3657782cf9e620058
Samir Sharif <samirsharif6969@gmail.com>
2025-11-12 02:03:13 -0500

    commit

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,12 +1,13 @@
 // lib/api/api.ts
 import axios, { AxiosError } from 'axios';
+export type ApiError = AxiosError<{ error: string }>;
 
-export type ApiError = AxiosError<{
-  error: string;
-}>;
+export const api = axios.create({
+  baseURL: 'https://notehub-api.goit.study',
+  withCredentials: true,
+});
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api` : '/api';
-
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 export const nextServer = axios.create({
   baseURL,
   withCredentials: true,
