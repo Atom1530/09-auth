a90c53d50975ef78155c9b45baa2c676d3d7d929
SamirSharif <samirsharif6969@gmail.com>
2025-11-12 19:23:11 -0500

    Update import path for ApiError in SignInPage

--- app/(auth routes)/sign-in/page.tsx
+++ app/(auth routes)/sign-in/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import css from './SignInPage.module.css';
 import { useState } from 'react';
 import { login, LoginData } from '@/lib/api/clientApi';
-import { ApiError } from '@/lib/api/api';
+import { ApiError } from '@/app/api/api';
 import { useAuthStore } from '@/lib/store/authStore';
 
 export default function SignInPage() {
